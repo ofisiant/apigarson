f2112882ff6c990af940605731fcaf7b7afb4d80
thebabayeff <iambabayeff@gmail.com>
2022-07-04 18:10:52 +0400

    Archive Works fixed in route .

--- routes/api.php
+++ routes/api.php
@@ -9,16 +9,8 @@ use App\Http\Controllers\Api\WorkController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
+
 
 Route::controller(AuthController::class)->group(function(){
     Route::post('register', 'register');
